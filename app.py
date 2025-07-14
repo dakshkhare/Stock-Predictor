8f44701b0eac32620e9acd0b757827ba52d47a21
dakshkhare <daksh.khare2004@gmail.com>
2025-07-14 21:55:48 +0530

    Update app.py with correct model path

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from keras.models import load_model
 import streamlit as st
 import matplotlib.pyplot as plt
 
-model = load_model('models/stock_model.keras')
+model = load_model('models\Stock Predictions Model.keras')
 
 st.header('Stock Predictor')
 
